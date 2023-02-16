e49505d26abe1c15254a371f194fb5ba19e1f78a
Starena <stef@ziza.be>
2023-02-16 16:33:37 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,52 @@
+input.onButtonPressed(Button.A, function () {
+    direction = input.compassHeading()
+    if (direction < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (direction < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (direction < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (direction < 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("SOS")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.temperature()))
+})
+let direction = 0
+input.calibrateCompass()
+basic.forever(function () {
+	
+})
